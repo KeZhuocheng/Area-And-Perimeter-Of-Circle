6156d4d4c844bbb24edef6e3b6d95b35b1f47b66
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-03 13:29:09 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double r,c,a;
+
+    printf("Type a number:");
+    scanf("%lf",&r);
+    a=3.1415926*r*r;
+    printf("The area of circle is :%.4f\n",a);
+    c=2*3.1415926*r;
+    printf("the zhouchang is :%.4f\n",c);
+    return 0;
+}
